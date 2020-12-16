eb78b2fa2243c84ceab4201db3227ecf8fdec01e
thomasyu888 <thomas.yu@sagebase.org>
2020-12-16 11:12:04 +0800

    Add

--- .github/actions/synapse-gh-to-wiki/src/main.py
+++ .github/actions/synapse-gh-to-wiki/src/main.py
@@ -7,15 +7,17 @@ synid = os.getenv("INPUT_SYNID")
 
 wiki_content = ""
 if os.path.exists("README.md"):
-    wiki_content += "# Description\n\n"
+    # wiki_content += "# Description\n\n"
     with open("README.md") as readme_f:
         wiki_content += readme_f.read()
 
 if os.path.exists("Dockerfile"):
     wiki_content += "\n\n"
     wiki_content += "# Dockerfile\n\n"
+    wiki_content += "```dockerfile\n"
     with open("Dockerfile") as readme_f:
         wiki_content += readme_f.read()
+    wiki_content += "\n```"
 
 wiki = synapseclient.Wiki(owner=synid, markdown=wiki_content)
 syn.store(wiki)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test-docker-push
+
+This is a test to see if the readme gets ported over.  Pretty cool stuff if you ask me!
